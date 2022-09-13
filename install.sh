2465f7d5986da8f14ce6bb932eb38404870dd1b3
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-13 18:54:09 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg update
+pkg upgrade
+pkg install php 
+pkg install openssh
+pkg install curl
+git clone https://github.com/kinghacker0/WishFish
+git clone https://github.com/EnzoGly/Termux-kali
+cd WishFish 
+chmod +x *
